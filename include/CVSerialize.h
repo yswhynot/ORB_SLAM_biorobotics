a6c2700e20cd8f18298960d6055a77cd92604978
yswhynot <yswhynot@gmail.com>
2016-08-22 09:43:11 -0400

    add cv::mat keypoint serialize

---
+++ include/CVSerialize.h
@@ -0,0 +1,90 @@
+#ifndef CVMAT_H_
+#define CVMAT_H_
+
+#include <opencv2/opencv.hpp>
+
+#include <boost/serialization/split_free.hpp>
+#include <boost/serialization/vector.hpp>
+
+#include <boost/archive/tmpdir.hpp>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/list.hpp>
+#include <boost/serialization/set.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/assume_abstract.hpp>
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/split_member.hpp>
+
+BOOST_SERIALIZATION_SPLIT_FREE(::cv::Mat);
+namespace boost {
+	namespace serialization {
+
+    /** Serialization support for cv::Mat */
+    template<class Archive>
+		void save(Archive & ar, const ::cv::Mat& m, const unsigned int version) {
+			size_t elem_size = m.elemSize();
+			size_t elem_type = m.type();
+
+			ar & m.cols;
+			ar & m.rows;
+			ar & elem_size;
+			ar & elem_type;
+
+			const size_t data_size = m.cols * m.rows * elem_size;
+			ar & boost::serialization::make_array(m.ptr(), data_size);
+		}
+
+    /** Serialization support for cv::Mat */
+    template<class Archive>
+		void load(Archive & ar, ::cv::Mat& m, const unsigned int version)
+		{
+			int cols, rows;
+			size_t elem_size, elem_type;
+
+			ar & cols;
+			ar & rows;
+			ar & elem_size;
+			ar & elem_type;
+
+			m.create(rows, cols, elem_type);
+
+			size_t data_size = m.cols * m.rows * elem_size;
+			ar & boost::serialization::make_array(m.ptr(), data_size);
+		}
+
+	}
+}
+
+BOOST_SERIALIZATION_SPLIT_FREE(::cv::KeyPoint);
+namespace boost {
+	namespace serialization {
+		template<class Archive>
+		void save(Archive& ar, const ::cv::KeyPoint& p, const unsigned int version) {
+			ar & p.pt.x;
+			ar & p.pt.y;
+			ar & p.size;
+			ar & p.angle;
+			ar & p.response;
+			ar & p.octave;
+			ar & p.class_id;
+		}
+
+		template<class Archive>
+		void load(Archive &ar, ::cv::KeyPoint& p, const unsigned int version) {
+			ar & p.pt.x;
+			ar & p.pt.y;
+			ar & p.size;
+			ar & p.angle;
+			ar & p.response;
+			ar & p.octave;
+			ar & p.class_id;
+		}
+	}
+}
+
+#endif /* CVMAT_SERIALIZE_HPP_ */
